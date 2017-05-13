8ce5e29ffbb44dcbc1637dfb89b0c0b2160eaf44
YufeiLiu <yufeiliu94@gmail.com>
2017-05-13 21:39:23 +0800

    desugar standard for-loop

--- phiplot/Language/PhiPlot/Desugar.hs
+++ phiplot/Language/PhiPlot/Desugar.hs
@@ -2,13 +2,16 @@ module Language.PhiPlot.Desugar ( desugar, desugarAll ) where
 
 import Language.PhiPlot.AST
 
+import Prelude hiding ( LT )
+
 desugarAll :: [AST] -> [AST]
 desugarAll = concatMap desugar 
 
 desugar :: AST -> [AST]
 desugar (Def fn args body)             = [ Def   fn      args            $ desugarAll body ]
-desugar (For var start stop step body) = [ For   var     start stop step $ desugarAll body ]
 desugar (If cond t f)                  = [ If    cond  ( desugarAll t )  ( desugarAll f )  ]
 desugar (While cond body)              = [ While cond                    $ desugarAll body ]
+desugar (For var start stop step body) = [ Assign var start
+                                         , While (Cmp LT (BEAtom (Var var)) (BEAtom stop)) $ desugarAll body ++ [Assign var (BinOp Plus (Var var) step)] ]
 desugar Void                           = [ ]
 desugar ast                            = [ ast ]
